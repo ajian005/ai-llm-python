5bfe276582f96168badeab260e2ebe7e835f196a
ajian005 <79331356@qq.com>
2025-08-06 17:08:30 +0800

    How to: return structured data from a model

--- LangChain/howToGuide_01_returnStructuredDataFromAModel.py
+++ LangChain/howToGuide_01_returnStructuredDataFromAModel.py
@@ -1,3 +1,395 @@
 """
    How to return structured data from a model  https://python.langchain.com/docs/how_to/structured_output/
 """
+
+# pip install -qU "langchain[openai]"
+
+import getpass
+import os
+
+# if not os.environ.get("OPENAI_API_KEY"):
+#   os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+from langchain.chat_models import init_chat_model
+
+# llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+llm = init_chat_model(
+    model="gpt-4o-mini", 
+    api_key =os.environ["DMXAPI_API_KEY"],
+    base_url="https://www.dmxapi.cn/v1",
+    model_provider="openai",  # 使用 OpenAI 模型提供者)
+)
+
+"""
+Pydantic class
+If we want the model to return a Pydantic object, we just need to pass in the desired Pydantic class. The key advantage of using Pydantic is that the model-generated output will be validated. Pydantic will raise an error if any required fields are missing or if any fields are of the wrong type.
+"""
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+# Pydantic
+class Joke(BaseModel):
+    """Joke to tell user."""
+
+    setup: str = Field(description="The setup of the joke")
+    punchline: str = Field(description="The punchline to the joke")
+    rating: Optional[int] = Field(
+        default=None, description="How funny the joke is, from 1 to 10"
+    )
+
+structured_llm = llm.with_structured_output(Joke)
+print(structured_llm)
+print("=====================================================")
+result2 = structured_llm.invoke("Tell me a joke about cats")
+print(result2)
+
+
+# TypedDict 
+from typing import Optional
+from typing_extensions import Annotated, TypedDict
+
+# TypedDict
+class Jake2(TypedDict):
+    """Joke to tell user."""
+
+    setup: Annotated[str, ..., "The setup of the joke"]
+
+    # Alternatively, we could have specified setup as:
+
+    # setup: str                    # no default, no description
+    # setup: Annotated[str, ...]    # no default, no description
+    # setup: Annotated[str, "foo"]  # default, no description
+    punchline: Annotated[str, ..., "The punchline to the joke"]
+    rating: Annotated[int, ..., "How funny the joke is, from 1 to 10"]
+
+structured_llm2 = llm.with_structured_output(Jake2)
+result2 = structured_llm2.invoke("Tell me a joke about cats")
+print("=================")
+print(result2)
+
+# JSON Schema
+json_schema = {
+    "title": "joke",
+    "description": "Joke to tell user.",
+    "type": "object",
+    "properties": {
+        "setup": {
+            "type": "string",
+            "description": "The setup of the joke",
+        },
+        "punchline": {
+            "type": "string",
+            "description": "The punchline to the joke",
+        },
+        "rating": {
+            "type": "integer",
+            "description": "How funny the joke is, from 1 to 10",
+            "default": None,
+        },
+    },
+    "required": ["setup", "punchline", "rating"],
+}
+structured_llm3 = llm.with_structured_output(json_schema)
+
+result3 = structured_llm3.invoke("Tell me a joke about cats")
+print("=================")
+print(result3)
+
+
+# Choosing between multiple schemas
+  # Using Pydantic
+from typing import Union
+
+
+class Joke3(BaseModel):
+   """Joke to tell user."""
+
+   setup: str = Field(description="The setup of the joke")
+   punchline: str = Field(description="The punchline to the joke")
+   rating: Optional[int] = Field(
+      default=None, description="How funny the joke is, from 1 to 10"
+   )
+
+
+class ConversationalResponse(BaseModel):
+    """Respond in a conversational manner. Be kind and helpful."""
+
+    response: str = Field(description="A conversational response to the user's query")
+
+
+class FinalResponse(BaseModel):
+    final_output: Union[Joke3, ConversationalResponse]
+
+
+structured_llm = llm.with_structured_output(FinalResponse)
+
+result = structured_llm.invoke("Tell me a joke about cats")
+print("=================")
+print(result)
+
+
+# Using TypedDict
+from typing import Optional, Union
+
+from typing_extensions import Annotated, TypedDict
+
+
+class Joke4(TypedDict):
+    """Joke to tell user."""
+
+    setup: Annotated[str, ..., "The setup of the joke"]
+    punchline: Annotated[str, ..., "The punchline of the joke"]
+    rating: Annotated[Optional[int], None, "How funny the joke is, from 1 to 10"]
+
+
+class ConversationalResponse4(TypedDict):
+    """Respond in a conversational manner. Be kind and helpful."""
+
+    response: Annotated[str, ..., "A conversational response to the user's query"]
+
+
+class FinalResponse4(TypedDict):
+    final_output: Union[Joke4, ConversationalResponse4]
+
+
+structured_llm = llm.with_structured_output(FinalResponse4)
+
+result = structured_llm.invoke("Tell me a joke about cats")
+print("=================")
+print(result)
+
+
+# Streaming
+from typing_extensions import Annotated, TypedDict
+
+
+# TypedDict
+class Joke(TypedDict):
+    """Joke to tell user."""
+
+    setup: Annotated[str, ..., "The setup of the joke"]
+    punchline: Annotated[str, ..., "The punchline of the joke"]
+    rating: Annotated[Optional[int], None, "How funny the joke is, from 1 to 10"]
+
+
+structured_llm = llm.with_structured_output(Joke)
+
+for chunk in structured_llm.stream("Tell me a joke about cats"):
+    print(chunk)
+
+# Few-shot prompting
+from langchain_core.prompts import ChatPromptTemplate
+
+system = """You are a hilarious comedian. Your specialty is knock-knock jokes. \
+Return a joke which has the setup (the response to "Who's there?") and the final punchline (the response to "<setup> who?").
+
+Here are some examples of jokes:
+
+example_user: Tell me a joke about planes
+example_assistant: {{"setup": "Why don't planes ever get tired?", "punchline": "Because they have rest wings!", "rating": 2}}
+
+example_user: Tell me another joke about planes
+example_assistant: {{"setup": "Cargo", "punchline": "Cargo 'vroom vroom', but planes go 'zoom zoom'!", "rating": 10}}
+
+example_user: Now about caterpillars
+example_assistant: {{"setup": "Caterpillar", "punchline": "Caterpillar really slow, but watch me turn into a butterfly and steal the show!", "rating": 5}}"""
+
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", "{input}")])
+
+few_shot_structured_llm = prompt | structured_llm
+result = few_shot_structured_llm.invoke("what's something funny about woodpeckers")
+print(result)
+
+from langchain_core.messages import AIMessage, HumanMessage, ToolMessage
+
+examples = [
+    HumanMessage("Tell me a joke about planes", name="example_user"),
+    AIMessage(
+        "",
+        name="example_assistant",
+        tool_calls=[
+            {
+                "name": "joke",
+                "args": {
+                    "setup": "Why don't planes ever get tired?",
+                    "punchline": "Because they have rest wings!",
+                    "rating": 2,
+                },
+                "id": "1",
+            }
+        ],
+    ),
+    # Most tool-calling models expect a ToolMessage(s) to follow an AIMessage with tool calls.
+    ToolMessage("", tool_call_id="1"),
+    # Some models also expect an AIMessage to follow any ToolMessages,
+    # so you may need to add an AIMessage here.
+    HumanMessage("Tell me another joke about planes", name="example_user"),
+    AIMessage(
+        "",
+        name="example_assistant",
+        tool_calls=[
+            {
+                "name": "joke",
+                "args": {
+                    "setup": "Cargo",
+                    "punchline": "Cargo 'vroom vroom', but planes go 'zoom zoom'!",
+                    "rating": 10,
+                },
+                "id": "2",
+            }
+        ],
+    ),
+    ToolMessage("", tool_call_id="2"),
+    HumanMessage("Now about caterpillars", name="example_user"),
+    AIMessage(
+        "",
+        tool_calls=[
+            {
+                "name": "joke",
+                "args": {
+                    "setup": "Caterpillar",
+                    "punchline": "Caterpillar really slow, but watch me turn into a butterfly and steal the show!",
+                    "rating": 5,
+                },
+                "id": "3",
+            }
+        ],
+    ),
+    ToolMessage("", tool_call_id="3"),
+]
+system = """You are a hilarious comedian. Your specialty is knock-knock jokes. \
+Return a joke which has the setup (the response to "Who's there?") \
+and the final punchline (the response to "<setup> who?")."""
+
+prompt = ChatPromptTemplate.from_messages(
+    [("system", system), ("placeholder", "{examples}"), ("human", "{input}")]
+)
+few_shot_structured_llm = prompt | structured_llm
+few_shot_structured_llm.invoke({"input": "crocodiles", "examples": examples})
+
+
+# (Advanced) Specifying the method for structuring outputs
+structured_llm = llm.with_structured_output(None, method="json_mode")
+
+structured_llm.invoke(
+    "Tell me a joke about cats, respond in JSON with `setup` and `punchline` keys"
+)
+
+# (Advanced) Raw outputs
+structured_llm = llm.with_structured_output(Joke, include_raw=True)
+structured_llm.invoke("Tell me a joke about cats")
+
+
+# Prompting and parsing model outputs directly
+# Using PydanticOutputParser
+from typing import List
+
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+
+class Person(BaseModel):
+    """Information about a person."""
+
+    name: str = Field(..., description="The name of the person")
+    height_in_meters: float = Field(
+        ..., description="The height of the person expressed in meters."
+    )
+
+
+class People(BaseModel):
+    """Identifying information about all people in a text."""
+
+    people: List[Person]
+
+
+# Set up a parser
+parser = PydanticOutputParser(pydantic_object=People)
+
+# Prompt
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Answer the user query. Wrap the output in `json` tags\n{format_instructions}",
+        ),
+        ("human", "{query}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+query = "Anna is 23 years old and she is 6 feet tall"
+
+print(prompt.invoke({"query": query}).to_string())
+
+chain = prompt | llm | parser
+
+chain.invoke({"query": query})
+
+# Custom Parsing =========================================================================
+import json
+import re
+from typing import List
+
+from langchain_core.messages import AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+
+
+class Person(BaseModel):
+    """Information about a person."""
+
+    name: str = Field(..., description="The name of the person")
+    height_in_meters: float = Field(
+        ..., description="The height of the person expressed in meters."
+    )
+
+
+class People(BaseModel):
+    """Identifying information about all people in a text."""
+
+    people: List[Person]
+
+
+# Prompt
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "Answer the user query. Output your answer as JSON that  "
+            "matches the given schema: \`\`\`json\n{schema}\n\`\`\`. "
+            "Make sure to wrap the answer in \`\`\`json and \`\`\` tags",
+        ),
+        ("human", "{query}"),
+    ]
+).partial(schema=People.schema())
+
+
+# Custom parser
+def extract_json(message: AIMessage) -> List[dict]:
+    """Extracts JSON content from a string where JSON is embedded between \`\`\`json and \`\`\` tags.
+
+    Parameters:
+        text (str): The text containing the JSON content.
+
+    Returns:
+        list: A list of extracted JSON strings.
+    """
+    text = message.content
+    # Define the regular expression pattern to match JSON blocks
+    pattern = r"\`\`\`json(.*?)\`\`\`"
+
+    # Find all non-overlapping matches of the pattern in the string
+    matches = re.findall(pattern, text, re.DOTALL)
+
+    # Return the list of matched JSON strings, stripping any leading or trailing whitespace
+    try:
+        return [json.loads(match.strip()) for match in matches]
+    except Exception:
+        raise ValueError(f"Failed to parse: {message}")
+      
+query = "Anna is 23 years old and she is 6 feet tall"
+print(prompt.format_prompt(query=query).to_string())
+chain = prompt | llm | extract_json
+chain.invoke({"query": query})
---
+++ LangChain/howToGuide_01_test.py
@@ -0,0 +1,54 @@
+import os
+from langchain.chat_models import init_chat_model
+
+# llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+llm = init_chat_model(
+    model="gpt-4o-mini", 
+    api_key =os.environ["DMXAPI_API_KEY"],
+    base_url="https://www.dmxapi.cn/v1",
+    model_provider="openai",  # 使用 OpenAI 模型提供者)
+)
+
+"""
+Pydantic class
+If we want the model to return a Pydantic object, we just need to pass in the desired Pydantic class. The key advantage of using Pydantic is that the model-generated output will be validated. Pydantic will raise an error if any required fields are missing or if any fields are of the wrong type.
+"""
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+# Pydantic
+class Joke(BaseModel):
+    """Joke to tell user."""
+
+    setup: str = Field(description="The setup of the joke")
+    punchline: str = Field(description="The punchline to the joke")
+    rating: Optional[int] = Field(
+        default=None, description="How funny the joke is, from 1 to 10"
+    )
+
+structured_llm = llm.with_structured_output(Joke)
+
+
+from langchain_core.prompts import ChatPromptTemplate
+
+system = """You are a hilarious comedian. Your specialty is knock-knock jokes. \
+Return a joke which has the setup (the response to "Who's there?") and the final punchline (the response to "<setup> who?").
+
+Here are some examples of jokes:
+
+example_user: Tell me a joke about planes
+example_assistant: {{"setup": "Why don't planes ever get tired?", "punchline": "Because they have rest wings!", "rating": 2}}
+
+example_user: Tell me another joke about planes
+example_assistant: {{"setup": "Cargo", "punchline": "Cargo 'vroom vroom', but planes go 'zoom zoom'!", "rating": 10}}
+
+example_user: Now about caterpillars
+example_assistant: {{"setup": "Caterpillar", "punchline": "Caterpillar really slow, but watch me turn into a butterfly and steal the show!", "rating": 5}}"""
+
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", "{input}")])
+
+few_shot_structured_llm = prompt | structured_llm
+result = few_shot_structured_llm.invoke("what's something funny about woodpeckers")
+print(result)
