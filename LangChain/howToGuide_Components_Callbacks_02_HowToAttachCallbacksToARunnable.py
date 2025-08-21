b8dd64a221ee6c31b514b949fc76b146bb330a89
ajian005 <79331356@qq.com>
2025-08-21 20:35:41 +0800

    Callbacks

---
+++ LangChain/howToGuide_Components_Callbacks_02_HowToAttachCallbacksToARunnable.py
@@ -0,0 +1,40 @@
+"""
+  How to attach callbacks to a runnable : https://python.langchain.com/docs/how_to/callbacks_attach/
+"""
+
+from typing import Any, Dict, List
+
+from langchain_anthropic import ChatAnthropic
+from langchain_core.callbacks import BaseCallbackHandler
+from langchain_core.messages import BaseMessage
+from langchain_core.outputs import LLMResult
+from langchain_core.prompts import ChatPromptTemplate
+
+
+class LoggingHandler(BaseCallbackHandler):
+    def on_chat_model_start(
+        self, serialized: Dict[str, Any], messages: List[List[BaseMessage]], **kwargs
+    ) -> None:
+        print("Chat model started")
+
+    def on_llm_end(self, response: LLMResult, **kwargs) -> None:
+        print(f"Chat model ended, response: {response}")
+
+    def on_chain_start(
+        self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs
+    ) -> None:
+        print(f"Chain {serialized.get('name')} started")
+
+    def on_chain_end(self, outputs: Dict[str, Any], **kwargs) -> None:
+        print(f"Chain ended, outputs: {outputs}")
+
+
+callbacks = [LoggingHandler()]
+llm = ChatAnthropic(model="claude-3-7-sonnet-20250219")
+prompt = ChatPromptTemplate.from_template("What is 1 + {number}?")
+
+chain = prompt | llm
+
+chain_with_callbacks = chain.with_config(callbacks=callbacks)
+
+chain_with_callbacks.invoke({"number": "2"})
---
+++ LangChain/howToGuide_Components_Callbacks_05_HowToUseCallbacksInAsyncEnvironments.py
@@ -0,0 +1,50 @@
+"""
+  How to use callbacks in async environments : https://python.langchain.com/docs/how_to/callbacks_async/
+"""
+
+import asyncio
+from typing import Any, Dict, List
+
+from langchain_anthropic import ChatAnthropic
+from langchain_core.callbacks import AsyncCallbackHandler, BaseCallbackHandler
+from langchain_core.messages import HumanMessage
+from langchain_core.outputs import LLMResult
+
+
+class MyCustomSyncHandler(BaseCallbackHandler):
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        print(f"Sync handler being called in a `thread_pool_executor`: token: {token}")
+
+
+class MyCustomAsyncHandler(AsyncCallbackHandler):
+    """Async callback handler that can be used to handle callbacks from langchain."""
+
+    async def on_llm_start(
+        self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
+    ) -> None:
+        """Run when chain starts running."""
+        print("zzzz....")
+        await asyncio.sleep(0.3)
+        class_name = serialized["name"]
+        print("Hi! I just woke up. Your llm is starting")
+
+    async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        """Run when chain ends running."""
+        print("zzzz....")
+        await asyncio.sleep(0.3)
+        print("Hi! I just woke up. Your llm is ending")
+
+
+# To enable streaming, we pass in `streaming=True` to the ChatModel constructor
+# Additionally, we pass in a list with our custom handler
+chat = ChatAnthropic(
+    model="claude-3-7-sonnet-20250219",
+    max_tokens=25,
+    streaming=True,
+    callbacks=[MyCustomSyncHandler(), MyCustomAsyncHandler()],
+)
+
+async def main():
+    await chat.agenerate([[HumanMessage(content="Tell me a joke")]])
+
+asyncio.run(main())
