6e426d0c547910ec82263a8df2ef881d0a672a81
ajian005 <79331356@qq.com>
2025-08-29 13:15:36 +0800

    BuildAnExtractionChain.py

---
+++ LangChain/UseCases/BuildAnExtractionChain.py
@@ -0,0 +1,125 @@
+"""
+  Build an Extraction Chain: https://python.langchain.com/docs/tutorials/extraction/
+"""
+
+""" ==================== The Schema ============================== """
+from typing import Optional, List
+from pydantic import BaseModel, Field
+
+class Person(BaseModel):
+    """Information about a person."""
+
+    # ^ Doc-string for the entity Person.
+    # This doc-string is sent to the LLM as the description of the schema Person,
+    # and it can help to improve extraction results.
+
+    # Note that:
+    # 1. Each field is an `optional` -- this allows the model to decline to extract it!
+    # 2. Each field has a `description` -- this description is used by the LLM.
+    # Having a good description can help improve extraction results.
+    name: Optional[str] = Field(default=None, description="The name of the person")
+    hair_color: Optional[str] = Field(
+        default=None, description="The color of the person's hair if known"
+    )
+    height_in_meters: Optional[str] = Field(
+        default=None, description="Height measured in meters"
+    )
+
+
+""" ==================== The Extractor ============================== """
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+# Define a custom prompt to provide instructions and any additional context.
+# 1) You can add examples into the prompt template to improve extraction quality
+# 2) Introduce additional parameters to take context into account (e.g., include metadata
+#    about the document from which the text was extracted.)
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are an expert extraction algorithm. "
+            "Only extract relevant information from the text. "
+            "If you do not know the value of an attribute asked to extract, "
+            "return null for the attribute's value.",
+        ),
+        # Please see the how-to about improving performance with
+        # reference examples.
+        # MessagesPlaceholder('examples'),
+        ("human", "{text}"),
+    ]
+)
+
+
+import os
+from langchain.chat_models import init_chat_model
+# 初始化 OpenAI 模型
+llm = init_chat_model(
+    model="gpt-4o-mini", 
+    api_key =os.environ["DMXAPI_API_KEY"],
+    base_url="https://www.dmxapi.cn/v1",
+    model_provider="openai",  # 使用 OpenAI 模型提供者)
+)
+
+structured_llm = llm.with_structured_output(schema=Person)
+
+text = "Alan Smith is 6 feet tall and has blond hair."
+prompt = prompt_template.invoke({"text": text})
+result = structured_llm.invoke(prompt)
+print(result)
+
+
+
+""" ==================== Multiple Entities ============================== """
+class Data(BaseModel):
+    """Extracted data about people."""
+
+    # Creates a model so that we can extract multiple entities.
+    people: List[Person]
+
+
+structured_llm = llm.with_structured_output(schema=Data)
+text = "My name is Jeff, my hair is black and i am 6 feet tall. Anna has the same color hair as me."
+prompt = prompt_template.invoke({"text": text})
+structured_llm.invoke(prompt)
+
+
+""" ==================== Reference examples ============================== """
+messages = [
+    {"role": "user", "content": "2 🦜 2"},
+    {"role": "assistant", "content": "4"},
+    {"role": "user", "content": "2 🦜 3"},
+    {"role": "assistant", "content": "5"},
+    {"role": "user", "content": "3 🦜 4"},
+]
+
+response = llm.invoke(messages)
+print(response.content)
+
+
+from langchain_core.utils.function_calling import tool_example_to_messages
+
+examples = [
+    (
+        "The ocean is vast and blue. It's more than 20,000 feet deep.",
+        Data(people=[]),
+    ),
+    (
+        "Fiona traveled far from France to Spain.",
+        Data(people=[Person(name="Fiona", height_in_meters=None, hair_color=None)]),
+    ),
+]
+
+
+messages = []
+
+for txt, tool_call in examples:
+    if tool_call.people:
+        # This final message is optional for some providers
+        ai_response = "Detected people."
+    else:
+        ai_response = "Detected no people."
+    messages.extend(tool_example_to_messages(txt, [tool_call], ai_response=ai_response))
+
+for message in messages:
+    message.pretty_print()
+
