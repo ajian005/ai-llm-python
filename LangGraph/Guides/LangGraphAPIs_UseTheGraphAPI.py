64937aaaf62e6902f4a4c170fc779a0dcb878f11
ajian005 <79331356@qq.com>
2025-09-08 20:07:21 +0800

    Graph API How to use the graph API

---
+++ LangGraph/Guides/LangGraphAPIs_UseTheGraphAPI.py
@@ -0,0 +1,17 @@
+"""
+    Graph API How to use the graph API : https://langchain-ai.github.io/langgraph/how-tos/graph-api/
+
+    This guide demonstrates the basics of LangGraph's Graph API. It walks through state, as well as composing common graph structures such as sequences, branches, and loops. 
+    It also covers LangGraph's control features, including the Send API for map-reduce workflows and the Command API for combining state updates with "hops" across nodes.
+"""
+
+"""
+    === Setup ====================
+    Install langgraph
+        pip install langgraph
+
+    Set up LangSmith for better debugging
+    Sign up for LangSmith to quickly spot issues and improve the performance of your LangGraph projects. 
+    LangSmith lets you use trace data to debug, test, and monitor your LLM apps built with LangGraph 
+    — read more about how to get started in the docs.
+"""
