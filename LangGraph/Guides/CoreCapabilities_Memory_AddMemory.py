41a2e0a6698dffd22cc5175f2d08dbc48eea70f5
ajian005 <79331356@qq.com>
2025-09-17 17:53:06 +0800

    Memory

---
+++ LangGraph/Guides/CoreCapabilities_Memory_AddMemory.py
@@ -0,0 +1,9 @@
+"""
+    Add and manage memory : https://langchain-ai.github.io/langgraph/how-tos/memory/add-memory/
+  
+    AI applications need memory to share context across multiple interactions. In LangGraph, you can add two types of memory:
+
+    Add short-term memory as a part of your agent's state to enable multi-turn conversations.
+    Add long-term memory to store user-specific or application-level data across sessions.
+
+"""
