38cfb759d13ccdec3d25495c992d6648b74429ab
ajian005 <79331356@qq.com>
2025-09-28 15:06:36 +0800

    视觉推理（QVQ）

---
+++ Alibaba-BaiLian/bailian-models/Multimodal/Visual_Reasoning_Demo.py
@@ -0,0 +1,77 @@
+"""
+    大模型服务平台-->百炼用户指南（模型）-->多模态 --> 视觉推理 :  https://help.aliyun.com/zh/model-studio/qvq
+
+    视觉推理（QVQ）: QVQ模型具有强大的视觉推理能力，它能够先输出思考过程，再输出回答内容。QVQ 模型当前仅支持通过流式输出方式调用。
+"""
+
+import os
+from openai import OpenAI
+import dashscope
+
+client = OpenAI(
+    # 若没有配置环境变量，请用阿里云百炼API Key将下行替换为：api_key="sk-xxx",
+    # 新加坡和北京地域的API Key不同。获取API Key：https://help.aliyun.com/zh/model-studio/get-api-key
+    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    # 以下是北京地域base-url，如果使用新加坡地域的模型，需要将base_url替换为：https://dashscope-intl.aliyuncs.com/compatible-mode/v1
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
+
+def visual_reasoning():
+    reasoning_content = ""  # 定义完整思考过程
+    answer_content = ""     # 定义完整回复
+    is_answering = False   # 判断是否结束思考过程并开始回复
+
+    # 创建聊天完成请求
+    completion = client.chat.completions.create(
+        model="qvq-max",  # 此处以 qvq-max 为例，可按需更换模型名称
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": "https://img.alicdn.com/imgextra/i1/O1CN01gDEY8M1W114Hi3XcN_!!6000000002727-0-tps-1024-406.jpg"
+                        },
+                    },
+                    {"type": "text", "text": "这道题怎么解答？"},
+                ],
+            },
+        ],
+        stream=True,
+        # 解除以下注释会在最后一个chunk返回Token使用量
+        # stream_options={
+        #     "include_usage": True
+        # }
+    )
+
+    print("\n" + "=" * 20 + "思考过程" + "=" * 20 + "\n")
+
+    for chunk in completion:
+        # 如果chunk.choices为空，则打印usage
+        if not chunk.choices:
+            print("\nUsage:")
+            print(chunk.usage)
+        else:
+            delta = chunk.choices[0].delta
+            # 打印思考过程
+            if hasattr(delta, 'reasoning_content') and delta.reasoning_content != None:
+                print(delta.reasoning_content, end='', flush=True)
+                reasoning_content += delta.reasoning_content
+            else:
+                # 开始回复
+                if delta.content != "" and is_answering is False:
+                    print("\n" + "=" * 20 + "完整回复" + "=" * 20 + "\n")
+                    is_answering = True
+                # 打印回复过程
+                print(delta.content, end='', flush=True)
+                answer_content += delta.content
+
+    # print("=" * 20 + "完整思考过程" + "=" * 20 + "\n")
+    # print(reasoning_content)
+    # print("=" * 20 + "完整回复" + "=" * 20 + "\n")
+    # print(answer_content)
+
+
+if __name__ == "__main__":
+    visual_reasoning()
