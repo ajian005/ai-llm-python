f5320ed07df7cb84ff647eb6e9da33bd4ab9021b
ajian005 <79331356@qq.com>
2026-01-15 11:37:10 +0800

    langChain

--- LangChain/chap001_01_fristTest.py
+++ LangChain/chap001_01_fristTest.py
@@ -1,7 +1,7 @@
 '''
-    langchain 入门指南（一）- 准备 API KEY   https://developer.aliyun.com/article/1586831?spm=a2c6h.24874632.expert-profile.150.1e375214WVEbY1
+    langchain 入门指南（一）- 准备 API KEY    https://developer.aliyun.com/article/1586831?spm=a2c6h.24874632.expert-profile.150.1e375214WVEbY1
     langchain 入门指南（二）- 如何跟大模型对话  https://developer.aliyun.com/article/1586834?spm=a2c6h.24874632.expert-profile.149.1e375214WVEbY1
-    langchain 入门指南（三）- token的计算  https://developer.aliyun.com/article/1586851?spm=a2c6h.24874632.expert-profile.146.1e375214WVEbY1
+    langchain 入门指南（三）- token的计算     https://developer.aliyun.com/article/1586851?spm=a2c6h.24874632.expert-profile.146.1e375214WVEbY1
 '''
 
 from langchain_openai import ChatOpenAI
